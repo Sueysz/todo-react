8beeb6b04d8e5bb517dac8813d99eb660ad56cf5
sueys <sueys@live.fr>
2023-09-13 04:47:20 +0200

    add button styled emotion & todoStyle

---
+++ src/styles/Buttons.ts
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+
+export const Btn = styled.button`
+margin-left:10px;
+    :hover{
+        cursor: pointer;
+    }
+`
---
+++ src/styles/TodoStyle.ts
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+export const H1 = styled.h1`
+    text-align:center;
+`
+
+export const InputContainer = styled.div`
+    display:flex;
+    justify-content:center;
+`
+export const TaskList = styled.div`
+    display:flex;
+    flex-direction:row;
+`
