04ee6491304fa79f284ae2385ef24fb013c24a0c
sueys <sueys@live.fr>
2023-09-22 04:32:07 +0200

    add local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { useState, ChangeEvent } from "react";
+import { useState, ChangeEvent, useEffect } from "react";
 import { TodoList } from "./component/TodoList.tsx"
 
 function App() {
   const [todo, setTodo] = useState('');
   const [todoTask, setTodoTask] = useState<string[]>([]);
+
+  useEffect(()=>{
+    const storedTodoTask= localStorage.getItem("todoTask");
+    if(storedTodoTask) {
+      setTodoTask(JSON.parse(storedTodoTask));
+    }
+  },[]);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setTodo(e.target.value);
@@ -11,8 +18,10 @@ function App() {
 
   const handleAddTask = () => {
     if (todo !== "") {
+      const newTodoTask = [...todoTask, todo];
       setTodoTask([...todoTask, todo]);
       setTodo("");
+      localStorage.setItem("todoTask", JSON.stringify(newTodoTask));
     }
   }
 
@@ -20,6 +29,8 @@ function App() {
     const newTodoTask = [...todoTask];
     newTodoTask.splice(index, 1);
     setTodoTask(newTodoTask)
+
+    localStorage.setItem("todoTask", JSON.stringify(newTodoTask));
   }
   return (
     <>
